e5868912e4e5ddb6e8f3a954dfb7538afe2e0671
David <78326757+daols1@users.noreply.github.com>
2022-09-22 06:42:17 +0100

    Installed all the dependecies

--- src/Components/BlogSec.jsx
+++ src/Components/BlogSec.jsx
@@ -18,7 +18,7 @@ function BlogSec({number}) {
   return (
     Object.keys(blog).length !== 0 ?
     <div className='container blog-sec'>
-        <div >
+        <div onLoad={<Loader />}>
             {blog.articles.slice(0, number).map((items) => 
             <div className='blog-rrs-card'>
                 <div className='rrs-img'><img src={items.urlToImage} alt="" className='rrs-img-itself' /></div>
